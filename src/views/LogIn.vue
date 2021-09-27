3f7957f54aad228876e451ba76fa89e5d4105d45
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-27 14:54:32 +0200

    Limit form width

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit="handleSubmit" class="form-group">
+  <form @submit="handleSubmit">
     <label>
       Login
       <input v-model="login" class="form-control" />
@@ -29,4 +29,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+form {
+  max-width: 400px;
+  margin: auto;
+}
+
+label {
+  display: block;
+}
+</style>
