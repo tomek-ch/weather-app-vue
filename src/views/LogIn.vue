89243f2e0d070fdf21749637553975750b589651
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-27 14:39:58 +0200

    Add a login form

---
+++ src/views/LogIn.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit="handleSubmit">
+    <label>
+      Login
+      <input v-model="login" />
+    </label>
+    <label>
+      Password
+      <input v-model="password" />
+    </label>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const login = ref("");
+    const password = ref("");
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log({ login, password });
+    };
+
+    return { handleSubmit, login, password };
+  },
+};
+</script>
+
+<style></style>
