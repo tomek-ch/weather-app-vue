44e432a9b8163a10364ed5ead3625561e224d697
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-28 11:40:41 +0200

    Style home link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
   <header>
-    <router-link to="/"><h1>Weather app</h1></router-link>
+    <router-link to="/"><h1 class="mb-3">Weather app</h1></router-link>
   </header>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 1.5em;
+}
+</style>
