28ea40118b9959cb9e4613bf02f7bcb38f98d3e1
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-28 08:15:09 +0200

    Clear input after submit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,6 +25,7 @@ export default defineComponent({
     const handleSubmit = (e: Event) => {
       e.preventDefault();
       context.emit("add-city", input.value);
+      input.value = "";
     };
 
     return { input, handleSubmit };
