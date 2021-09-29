e64c1c152ede1bbb5a77cc12da3305e596c6dc5c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-29 08:17:53 +0200

    Refactor auth handling to use a listener

--- src/auth/store.ts
+++ src/auth/store.ts
@@ -3,30 +3,39 @@ import {
   createUserWithEmailAndPassword,
   User,
   signInWithEmailAndPassword,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { ref } from "vue";
 
 export const user = ref<User | null>(null);
 const auth = getAuth();
 
+onAuthStateChanged(auth, (userData) => {
+  if (userData) {
+    user.value = userData;
+  } else {
+    user.value = null;
+  }
+});
+
 export const register = (email: string, password: string) => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      user.value = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  createUserWithEmailAndPassword(auth, email, password);
+  // .then((userCredential) => {
+  //   user.value = userCredential.user;
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  // });
 };
 
 export const logIn = (email: string, password: string) => {
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      user.value = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  signInWithEmailAndPassword(auth, email, password);
+  // .then((userCredential) => {
+  //   user.value = userCredential.user;
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  // });
 };
