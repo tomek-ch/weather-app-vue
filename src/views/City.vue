07f2a7ec89a14529a636815993714acf128b4cb5
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-29 15:05:44 +0200

    Add error handling to city page

--- src/views/City.vue
+++ src/views/City.vue
@@ -13,6 +13,11 @@
     <h3 class="my-5">Humidity</h3>
     <Chart :data="humidityData" unit="%" color="#007bff" />
   </div>
+  <div v-else-if="error">
+    <h1>404</h1>
+    <p>City not found</p>
+  </div>
+  <Spinner v-else />
 </template>
 
 <script setup lang="ts">
@@ -22,15 +27,19 @@ import { computed, ref, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 import Chart from "@/components/Chart.vue";
 import getTime from "@/utils/getTime";
+import Spinner from "@/components/Spinner.vue";
 
 const route = useRoute();
 const city = ref<CityWeather | null>(null);
+const error = ref(false);
 
 watchEffect(async () => {
   if (route.params.name) {
     const data = await getCityByName(route.params.name as string);
     if (data) {
       city.value = data;
+    } else {
+      error.value = true;
     }
   }
 });
