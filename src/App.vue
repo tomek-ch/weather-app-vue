1c16e1444e9c41ff7ba375c833df089a1c42f7d0
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-29 11:14:14 +0200

    Use a spinner as a loading indicator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container mx-auto p-4">
-    <div v-if="expectSignIn && !user">
-      Loading...
+    <div class="d-flex justify-content-center" v-if="expectSignIn && !user">
+      <div class="spinner-border mt-5 " role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
     </div>
     <template v-else>
       <Header />
