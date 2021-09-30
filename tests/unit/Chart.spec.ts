8bf303bd6e70c4083869947e57cccc5fbbf86dff
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-30 13:30:42 +0200

    Test chart bar heights

--- tests/unit/Chart.spec.ts
+++ tests/unit/Chart.spec.ts
@@ -2,26 +2,26 @@ import { shallowMount } from "@vue/test-utils";
 import Chart from "@/components/Chart.vue";
 
 const data = [
-  { value: 30, label: "1" },
-  { value: 50, label: "2" },
-  { value: 100, label: "3" },
+  { value: 21, label: "1" },
+  { value: 37, label: "2" },
+  { value: 44, label: "3" },
 ];
 
+const max = Math.max(...data.map(({ value }) => value));
 const unit = "%";
 const color = "blue";
 
-const wrapper = shallowMount(Chart, {
-  props: {
-    data,
-    unit,
-    color,
-  },
-});
-
-const bars = wrapper.findAll(".bar");
+const wrapper = shallowMount(Chart, { props: { data, unit, color } });
+const bars = wrapper.findAll<HTMLElement>(".bar");
 
 describe("Chart.vue", () => {
   it("renders the correct number of bars", () => {
     expect(bars.length).toBe(data.length);
+  });
+
+  it("renders bars of correct heights", () => {
+    bars.forEach(({ element: { style: { height } } }, idx) =>
+      expect(height).toBe(`${(data[idx].value / max) * 100}%`)
+    );
   });
 });
