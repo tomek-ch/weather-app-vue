0afb1de88d493bbe28cc1860e709340848de6cf9
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-30 10:07:17 +0200

    Remove comments from firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA3FVNoYIBTJGz4SHlOOgEEnfO1iFqJswc",
   authDomain: "weather-app-9d6a9.firebaseapp.com",
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: "1:970750008162:web:9865b661f5c96770dfaa3b",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
