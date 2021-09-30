d9437b28ee3d973dfdef14fb933b55a246714f85
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-30 11:40:06 +0200

    Test auth form button label

---
+++ tests/unit/AuthForm.spec.ts
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import AuthForm from "@/components/AuthForm.vue";
+
+describe("Alert.vue", () => {
+  it("renders a button with appropriate label", () => {
+    const label = "test";
+
+    const wrapper = shallowMount(AuthForm, {
+      props: { label },
+    });
+
+    const text = wrapper.find("button").text();
+    expect(text).toMatch(label);
+  });
+});
