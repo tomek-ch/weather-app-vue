069f0e6fbab46d4bd6ea60ddd5ed5d74e026f2d2
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-30 11:50:07 +0200

    Test auth form password validation

--- tests/unit/AuthForm.spec.ts
+++ tests/unit/AuthForm.spec.ts
@@ -17,4 +17,13 @@ describe("Alert.vue", () => {
     const wrapper = shallowMount(AuthForm, {});
     expect(wrapper.find("button").element.disabled).toBe(true);
   });
+
+  it("is disabled when the password is too short", () => {
+    const wrapper = shallowMount(AuthForm, {});
+
+    wrapper.find("input").setValue("test@test.com");
+    wrapper.find('input[type="password"]').setValue("12345");
+
+    expect(wrapper.find("button").element.disabled).toBe(true);
+  });
 });
