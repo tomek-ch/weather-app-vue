7d4ed974545a62002901ca622411b341036b91ce
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-30 09:09:00 +0200

    Move app state down to the route that needs it

--- src/App.vue
+++ src/App.vue
@@ -3,67 +3,16 @@
     <Spinner v-if="expectSignIn && !user" />
     <template v-else>
       <Header />
-      <router-view
-        @add-city="addCity"
-        @delete-city="deleteCity"
-        :cityList="cityList"
-        :weatherData="weatherData"
-      />
+      <router-view />
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
 import "./firebase";
-import { ref, watchEffect } from "vue";
-import CityWeather from "./types/CityWeather";
-import getCities from "./utils/getCities";
-import { getCityByName } from "./utils/getCity";
-import Header from "./components/Header.vue";
 import { expectSignIn, user } from "./auth/store";
-import { addUserCity, deleteUserCity, getUserCities } from "./db";
+import Header from "./components/Header.vue";
 import Spinner from "./components/Spinner.vue";
-
-const cityList = ref<number[]>([]);
-const weatherData = ref<CityWeather[]>([]);
-
-// Sync the list of user's cities
-watchEffect(() => {
-  if (user.value) {
-    getUserCities(user.value.uid)
-      .then((userCities) => (cityList.value = userCities))
-      .then(async () => (weatherData.value = await getCities(cityList.value)))
-      .catch(console.log);
-  } else {
-    cityList.value = [];
-    weatherData.value = [];
-  }
-});
-
-const addCity = async (name: string, handleError: (msg: string) => void) => {
-  const data = await getCityByName(name);
-  if (
-    user.value &&
-    data &&
-    !weatherData.value.find(({ id }) => data.id === id)
-  ) {
-    addUserCity(user.value.uid, data.id);
-    cityList.value.push(data.id);
-    weatherData.value.push(data);
-  } else if (!data) {
-    handleError("Could not find that city");
-  } else {
-    handleError("City is already on the list");
-  }
-};
-
-const deleteCity = (cityId: number) => {
-  if (user.value) {
-    deleteUserCity(user.value.uid, cityId);
-    weatherData.value = weatherData.value.filter(({ id }) => id !== cityId);
-    cityList.value = cityList.value.filter((id) => id !== cityId);
-  }
-};
 </script>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <form
-      @submit.prevent="
-        $emit('add-city', input, handleError);
-        input = '';
-      "
-      class="mb-3"
-    >
+    <form @submit.prevent="addCity" class="mb-3">
       <input v-model="input" class="form-control" placeholder="London" />
       <button
         class="btn btn-primary"
@@ -21,30 +15,66 @@
     </form>
     <Alert :msg="error" variant="warning" />
     <div v-for="city in weatherData" :key="city.id" class="list-group">
-      <CityItem :city="city" @delete-city="$emit('delete-city', $event)" />
+      <CityItem :city="city" @delete-city="deleteCity" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, PropType } from "vue";
-import CityWeather from "@/types/CityWeather";
-import CityItem from "@/components/CityItem.vue";
-import usePrivateRoute from "@/composables/usePrivateRoute";
-import useError from "@/composables/useError";
 import Alert from "@/components/Alert.vue";
-
-defineProps({
-  cityList: { type: Array as PropType<Array<number>>, required: true },
-  weatherData: {
-    type: Array as PropType<Array<CityWeather>>,
-    required: true,
-  },
-});
-defineEmits(["add-city", "delete-city"]);
+import CityItem from "@/components/CityItem.vue";
+import useError from "@/composables/useError";
+import usePrivateRoute from "@/composables/usePrivateRoute";
+import CityWeather from "@/types/CityWeather";
+import { ref, watchEffect } from "vue";
+import { user } from "@/auth/store";
+import { addUserCity, deleteUserCity, getUserCities } from "@/db";
+import getCities from "@/utils/getCities";
+import { getCityByName } from "@/utils/getCity";
 
 const input = ref("");
 const { error, handleError } = useError();
+const cityList = ref<number[]>([]);
+const weatherData = ref<CityWeather[]>([]);
+
+// Sync the list of user's cities
+watchEffect(() => {
+  if (user.value) {
+    getUserCities(user.value.uid)
+      .then((userCities) => (cityList.value = userCities))
+      .then(async () => (weatherData.value = await getCities(cityList.value)))
+      .catch(console.log);
+  } else {
+    cityList.value = [];
+    weatherData.value = [];
+  }
+});
+
+const addCity = async () => {
+  const data = await getCityByName(input.value);
+  if (
+    user.value &&
+    data &&
+    !weatherData.value.find(({ id }) => data.id === id)
+  ) {
+    addUserCity(user.value.uid, data.id);
+    cityList.value.push(data.id);
+    weatherData.value.push(data);
+  } else if (!data) {
+    handleError("Could not find that city");
+  } else {
+    handleError("City is already on the list");
+  }
+};
+
+const deleteCity = (cityId: number) => {
+  if (user.value) {
+    deleteUserCity(user.value.uid, cityId);
+    weatherData.value = weatherData.value.filter(({ id }) => id !== cityId);
+    cityList.value = cityList.value.filter((id) => id !== cityId);
+  }
+};
+
 usePrivateRoute();
 </script>
 
