fb3bf83f79c75aef07c33f8b9389237d83127483
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-10-01 09:45:32 +0200

    Add icon to alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,9 +1,15 @@
 <template>
-  <div v-if="msg" :class="`alert alert-${variant}`" role="alert">
+  <div
+    v-if="msg"
+    :class="`alert alert-${variant} d-flex align-items-center`"
+    role="alert"
+  >
+    <AlertIcon class="mr-2" />
     {{ msg }}
   </div>
 </template>
 
 <script setup lang="ts">
+import AlertIcon from "@/icons/Alert.vue";
 defineProps({ msg: String, variant: String });
 </script>
