4a8352d4509bd60d4ad066a4f1bc9690b8b0aa21
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-10-01 08:59:47 +0200

    Test header email rendering

--- tests/unit/Header.spec.ts
+++ tests/unit/Header.spec.ts
@@ -2,13 +2,22 @@ import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 import Header from "@/components/Header.vue";
 
 const stubs = { RouterLink: RouterLinkStub };
+const user = { email: "test email" };
+const wrapperWithUser = shallowMount(Header, {
+  props: { user },
+  global: { stubs },
+});
 
 describe("Header.vue", () => {
   it("renders auth links when there is no user", () => {
-    const wrapper = shallowMount(Header, {
+    const wrapperWithoutUser = shallowMount(Header, {
       props: { user: null },
       global: { stubs },
     });
-    expect(wrapper.findAllComponents(RouterLinkStub).length).toBe(3);
+    expect(wrapperWithoutUser.findAllComponents(RouterLinkStub).length).toBe(3);
+  });
+
+  it("renders user's email", () => {
+    expect(wrapperWithUser.text()).toContain(user.email);
   });
 });
