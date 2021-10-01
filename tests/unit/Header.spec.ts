57f20bcbd82c610fed4bb9172e9bc19133043f6e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-10-01 09:09:49 +0200

    Test header for not rendering a sign out button

--- tests/unit/Header.spec.ts
+++ tests/unit/Header.spec.ts
@@ -3,18 +3,24 @@ import Header from "@/components/Header.vue";
 
 const stubs = { RouterLink: RouterLinkStub };
 const user = { email: "test email" };
+
 const wrapperWithUser = shallowMount(Header, {
   props: { user },
   global: { stubs },
 });
 
+const wrapperWithoutUser = shallowMount(Header, {
+  props: { user: null },
+  global: { stubs },
+});
+
 describe("Header.vue", () => {
   it("renders auth links when there is no user", () => {
-    const wrapperWithoutUser = shallowMount(Header, {
-      props: { user: null },
-      global: { stubs },
-    });
     expect(wrapperWithoutUser.findAllComponents(RouterLinkStub).length).toBe(3);
+  });
+
+  it("doesn't render a sign out button when there is no user", () => {
+    expect(wrapperWithoutUser.find("button").exists()).toBe(false);
   });
 
   it("renders user's email", () => {
